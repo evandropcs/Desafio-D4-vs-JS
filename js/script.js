a921f94b281fdda8b6c305f3a2d5519b9cd6af57
evandropcs <evandropcs@hotmail.com>
2022-08-22 16:39:42 -0300

    layout css, logica letra certa posição errada

--- js/script.js
+++ js/script.js
@@ -9,6 +9,9 @@ let indice = Math.floor(Math.random() * palavrasValidas.length)
 // let palavraDoDia = palavrasValidas[indice]
 palavraDoDia = 'OSSOS'
 
+// VARIAVEL PARA SALVAR VALOR DE PALAVRA DO DIA
+var palavraDoDiaAUX = palavraDoDia
+
 let linha = 1
 
 let entrada = []
@@ -71,8 +74,11 @@ function removeLetra() {
 
 function validarEntrada() {
     entrada = entrada.join('')
+
+    // debugger
+    
     for (var i = 0; i < palavraDoDia.length; i++){
-        
+        // debugger
         // Letra correta na posição correta
         if (palavraDoDia[i] == (entrada[i]) ){
             let elId = `l${linha}c${i+1}`
@@ -80,15 +86,56 @@ function validarEntrada() {
             l.classList.add('validado')
             l.classList.add("animate__animated")
             l.classList.add("animate__flipInX")
-
+            entrada = entrada.replace(entrada[i], '@')
+            palavraDoDia = palavraDoDia.replace(palavraDoDia[i], '!')
         }
 
         // Letra correta na posição errada correta
         else if (palavraDoDia.includes(entrada[i])){
             let elId = `l${linha}c${i+1}`
             var l = document.getElementById(elId)
-            l.classList.add('posicao-errada')
+
+            // CONTA LETRA NA PALAVRA DO DIA
+
+            var letraRepetidaDia = 0
+
+            for (x of palavraDoDia){
+                if (x == entrada[i]){
+                    letraRepetidaDia += 1
+                }
+            }
+
+            // CONTA LETRA NA ENTRADA
+            var letraRepetidaEntrada = 0
+
+            for (x of entrada){
+                if (x == entrada[i]){
+                    letraRepetidaEntrada += 1
+                }
+            }
+
+            if (letraRepetidaEntrada < letraRepetidaDia){
+                l.classList.add('posicao-errada')
+                letraRepetidaDia = 0
+                letraRepetidaEntrada = 0
+                entrada = entrada.replace(entrada[i], '@')
+            } else if (letraRepetidaEntrada == letraRepetidaDia){
+                l.classList.add('posicao-errada')
+                letraRepetidaDia = 0
+                letraRepetidaEntrada = 0
+                palavraDoDia = palavraDoDia.replace(palavraDoDia.indexOf(entrada[i]),'@')
+                entrada = entrada.replace(entrada[i], '@')
+            } 
+            
+            else{
+                l.classList.add('invalido')
+                letraRepetidaDia = 0
+                letraRepetidaEntrada = 0
+                entrada = entrada.replace(entrada[i], '@')
+            }
+
         } 
+        
         
         // Letra não existe
         else{
@@ -110,7 +157,11 @@ function validarEntrada() {
     if (linha === 6 && palavraDoDia != entrada){
         setTimeout(fracasso, 600)
     }
+
+    palavraDoDia = palavraDoDiaAUX
 }
+
+
 
 function sucesso (){
     alert('Parabéns, você venceu!')
